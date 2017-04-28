8001983f5f61b5e8da21f902b39595af1c12919f
MahmoudAboHamed <sls@DESKTOP-NHF2DCO>
2017-04-28 18:04:34 +0300

    update comic Book detail view

--- FirstWebApp/FirstWebApp/Controllers/ComicBookController.cs
+++ FirstWebApp/FirstWebApp/Controllers/ComicBookController.cs
@@ -11,6 +11,16 @@ namespace FirstWebApp.Controllers
        
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Hero";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>The best Book in World <strong>HaHaHaHa</strong></p> ";
+            ViewBag.Artists = new string[]
+            {
+        "script : ana",
+        "Pencils : ana",
+        "Inks : ana",
+            };
+
             return View();
         }
     }
